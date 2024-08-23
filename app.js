555e926c0cd0d344585373f70940a8c26209527d
Alex341K <manuelrangel@gmail.com>
2024-08-22 23:44:59 -0600

    uso de prompt

---
+++ app.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Escribe tu nombre:");
+let edad = prompt("Escribe tu edad:");
+let estatura = prompt ("Ingresa tu estatura en centimetros:");
+
+console.log("Tu nombres es: "+ nombre + " y tienes "+ parseInt(edad) + " años");
+
+console.log("Tu estatura es: "+ parseInt(estatura)/100 +" metros");
+
+console.log("De nada por la info")
